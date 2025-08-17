64695ed7a1f90eb3dba87837eaec770df9a2d5dc
Rahul Ranjan <rahul@Rahul-MacBook-Air.local>
2025-08-17 19:40:13 +0530

    config to enable and disable MCP client

--- src/main/java/com/rr/ai/mcp/api/ApiSampleForMCPClientController.java
+++ src/main/java/com/rr/ai/mcp/api/ApiSampleForMCPClientController.java
@@ -4,6 +4,7 @@ import com.rr.ai.mcp.service.AIPromptWithMCPServerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/mcp")
 @Slf4j
+@ConditionalOnProperty(name = "spring.ai.mcp.client.enabled", havingValue = "true", matchIfMissing = true)
 public class ApiSampleForMCPClientController {
 
     private final AIPromptWithMCPServerService aiPromptWithMCPServerService;
--- src/main/java/com/rr/ai/mcp/service/AIPromptWithMCPServerService.java
+++ src/main/java/com/rr/ai/mcp/service/AIPromptWithMCPServerService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Service
 @Slf4j
+@ConditionalOnProperty(name = "spring.ai.mcp.client.enabled", havingValue = "true", matchIfMissing = true)
 public class AIPromptWithMCPServerService {
 
     private final ChatClient chatClient;
